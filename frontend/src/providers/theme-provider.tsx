1b4a3a1481b48d669c406d105f015506b2540c5e
Mujeebbot <hdtrailers951@gmail.com>
2026-07-30 17:06:58 +0100

    Replace frontend with redesigned LegacyX app

--- frontend/src/providers/theme-provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import * as React from "react";
-
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return <div className="dark bg-[#08090C] min-h-screen text-white">{children}</div>;
-}
